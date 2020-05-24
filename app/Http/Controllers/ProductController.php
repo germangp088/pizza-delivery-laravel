385f18d79c076d843c0079dc77444aee8c55b539
German Dario <germangp088@gmail.com>
2020-05-24 14:07:20 -0300

    Retrieve product list

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -14,6 +15,8 @@ class ProductController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $products = Product::all();
+        $productList =  array('products' => $products);
+        return json_encode($productList);
     }
 }
