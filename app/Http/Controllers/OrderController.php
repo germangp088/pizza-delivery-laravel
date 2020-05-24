016f6a29f189d1e2a7b9e1272addddeb38e097fe
German Dario <germangp088@gmail.com>
2020-05-24 19:43:11 -0300

    Validate and persist customer

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Order;
+use App\Customer;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -20,11 +20,22 @@ class OrderController extends Controller
     /**
      * Show the form for creating a new resource.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $customer = new Customer();
+        $customerRequest = json_decode(json_encode($request->customer));
+        if(!$customer->isValid($customerRequest)){
+            return response()->json([
+                "errorMessage" => "Bad Request"
+              ], 400);
+        } else {
+            $customer->persist($customerRequest);
+        }
+
+        return response()->json($customer->id);
     }
 
     /**
